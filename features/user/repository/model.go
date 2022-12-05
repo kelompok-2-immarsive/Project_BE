5c34c2c7f9425ab7eb38681d331539ed64287522
fajar <frizky861@gmail.com>
2022-12-05 13:56:29 +0700

    jangan di ubah ubah kerangka

--- features/user/repository/model.go
+++ features/user/repository/model.go
@@ -1,1 +1,13 @@
 package repository
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	FullName string `gorm:"type:varchar(50)"`
+	Password string `gorm:"type:varchar(15)"`
+	Email    string
+	Phone    string
+	Address  string
+	Role     string
+}
